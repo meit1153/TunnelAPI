964e1f4903a5c609b2a8a7b8009e9e7bc7f485a6
Meit Maheshwari <38315313+meit1153@users.noreply.github.com>
2019-10-31 20:11:33 +0530

    Update admin.py

--- whitelist/apps/api/admin.py
+++ whitelist/apps/api/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import UserKey, AvailablePort, AssignPort
 
 # Register your models here.
+
+class UserKeyAdmin(admin.ModelAdmin):
+  fields = ('user', 'key')
+  
+class AvailablePortAdmin(admin.ModelAdmin):
+  fields = ('portno', 'is_available')
+
+class AssignPortAdmin(admin.ModelAdmin):
+  fields = ('user', 'key', 'assigned_port', 'user_ip', 'user_port', 'is_enable')
+
+
+admin.site.register(UserKey, UserKeyAdmin)
+admin.site.register(AvailablePort, AvailablePortAdmin)
+admin.site.register(AssignPort, AssignPortAdmin)
